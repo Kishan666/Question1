a865cfd4b360e533a53c3956f759f73799a5109b
Kishan666 <kishanparsotam69@gmail.com>
2017-03-11 15:46:48 +0200

    Added HalloWorld Files

---
+++ HalloWorld/Java/src/main/java/Hello/HelloWord.java
@@ -0,0 +1,15 @@
+package Hello;
+
+/**
+ * Created by Kishan on 2017-03-11.
+ */
+public class HelloWord {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+
+    }
+
+
+}
---
+++ HalloWorld/Java/src/test/java/HalloWorldTest.java
@@ -0,0 +1,7 @@
+package java;
+
+/**
+ * Created by Kishan on 2017-03-11.
+ */
+public class HalloWorldTest {
+}
